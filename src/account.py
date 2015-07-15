967ba116f8865b73a21e70a76e710178c8515643
C3P33N <c3p33n@gmail.com>
2015-07-15 22:22:06 +0200

    Fixed error with dynamic interest rate, need to fix unit tests

--- src/account.py
+++ src/account.py
@@ -2,34 +2,38 @@ __author__ = 'krzysztofduda'
 
 
 class NoFundsAvailableException(Exception):
-
     def __init__(self, *args, **kwargs):
         super(NoFundsAvailableException, self).__init__(*args, **kwargs)
+        self.amount = args[0]
+        self.balance = args[1]
 
     def __repr__(self):
-        return "No funds available at this time, sorry!"
+        return "No funds available at this time, sorry! Amount is %s, and account balance is %s." % (self.amount, self.balance)
 
 
 class Account():
-
     def __init__(self):
         self.available_bills = (10, 20, 50, 100, 200)
         self.balance = 1000
         self.interest = 0.05
 
     def withdrawFunds(self, amount=None, native_client=None):
-        received_bill = list()
-        amount_with_interest = amount*self.interest
-        for x in reversed(self.available_bills):
-            if amount-x>=0 :
-                received_bill.append(x)
-                if native_client:
-                    amount -= x
-                else:
-                    amount -= (x+amount_with_interest)
-                    if amount <=0:
-                        raise NoFundsAvailableException
-                print amount_with_interest
 
-        return received_bill
+        bills_from_atm = list()
+        amount_with_interest = amount - amount * self.interest
+
+        if not native_client:
+            amount = amount_with_interest
+
+        self.balance -= amount
+
+        for x in reversed(self.available_bills):
+            if amount - x >= 0:
+                bills_from_atm.append(x)
+                amount -= x
+
+        if amount < 0 or self.balance < 0:
+            raise NoFundsAvailableException(amount, self.balance)
+
+        return bills_from_atm
 
--- test/test_account.py
+++ test/test_account.py
@@ -4,6 +4,8 @@ from src.account import Account
 __author__ = 'krzysztofduda'
 
 @Test
-class withdraw_funds():
+class withdraw_funds_native_client():
     account = Account()
-    print account.withdrawFunds(230,False)
+    account.withdrawFunds(amount=0, native_client=True)
+    account.withdrawFunds(amount=200, native_client=True)
+    account.withdrawFunds(amount=750, native_client=True)
